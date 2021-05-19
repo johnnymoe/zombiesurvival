ea859a6f7e49688f2fdae25602a3708b16d9cfba
johnnymoe <moonchickendograt@gmail.com>
2021-05-19 00:35:09 -0500

    Electrohammer: Increased HealStrength by 10 points.

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_electrohammer.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_electrohammer.lua
@@ -22,7 +22,7 @@ end
 SWEP.Base = "weapon_zs_hammer"
 
 SWEP.MeleeDamage = 19 --40
-SWEP.HealStrength = 1.4
+SWEP.HealStrength = 2.4
 
 SWEP.ViewModel = "models/weapons/v_hammer/c_hammer.mdl"
 SWEP.WorldModel = "models/weapons/w_hammer.mdl"
