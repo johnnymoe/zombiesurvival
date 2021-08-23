672e005fa06b1243706b24933dc78b81f3be7489
johnnymoe <moonchickendograt@gmail.com>
2021-08-23 10:07:48 -0500

    Gluon Damage Buff 13.25 -> 16

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_gluon/shared.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_gluon/shared.lua
@@ -11,7 +11,7 @@ SWEP.ShowViewModel = false
 SWEP.ShowWorldModel = false
 SWEP.UseHands = true
 
-SWEP.Primary.Damage = 17
+SWEP.Primary.Damage = 16
 SWEP.Primary.NumShots = 1
 SWEP.Primary.Delay = 0.09
 SWEP.Primary.KnockbackScale = 0.1
